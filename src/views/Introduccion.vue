6ec3030772cca1959c61b51e7b8de1fb716b0975
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2026-02-05 21:23:59 -0500

    avance_CFA

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -20,7 +20,7 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/W_Me3HVi8WU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/yIBJ2AkVxpY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
         figcaption Video. Alistamiento y mantenimiento preventivo de equipos
 
 </template>
